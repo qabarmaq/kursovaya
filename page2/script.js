ecc2db4e3b9466e7d227b05b798a08d54925642d
qabarmaq <kekichybiza@gmail.com>
2025-04-22 12:36:26 +0300

    Add files via upload

---
+++ page2/script.js
@@ -0,0 +1,23 @@
+
+function snow() {
+    document.querySelector('.map2none').classList.toggle('map2')
+    document.querySelector('.check1').classList.toggle('checkon')
+}
+
+
+function sekv()  {
+    document.querySelector('.map3none').classList.toggle('map3')
+    document.querySelector('.check2').classList.toggle('checkon')
+}
+
+
+function swamp()  {
+    document.querySelector('.map4none').classList.toggle('map4')
+    document.querySelector('.check3').classList.toggle('checkon')
+}
+
+
+function obel()  {
+    document.querySelector('.map5none').classList.toggle('map5')
+    document.querySelector('.check4').classList.toggle('checkon')
+}
---
+++ page4/script.js
@@ -0,0 +1,20 @@
+
+function weapon() {
+    document.querySelector('.weapoon').style.display="block"
+    document.querySelector('.armoor').style.display="none"
+    document.querySelector('.rees').style.display="none"
+
+}
+function armor() {
+    document.querySelector('.weapoon').style.display="none"
+    document.querySelector('.armoor').style.display="block"
+    document.querySelector('.rees').style.display="none"
+
+}
+function res() {
+    document.querySelector('.weapoon').style.display="none"
+    document.querySelector('.armoor').style.display="none"
+    document.querySelector('.rees').style.display="block"
+    
+
+}
